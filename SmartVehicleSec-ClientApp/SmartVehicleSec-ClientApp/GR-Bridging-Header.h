fcc6dd27d10ffa50c9274518c01dd87d11132820
Jordan Hubbard <jordan_hubbard@apple.com>
2017-07-26 21:41:49 -0700

    Added Security Socket Client module to the repo
    Socket class added to app

---
+++ SmartVehicleSec-ClientApp/SmartVehicleSec-ClientApp/GR-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  NMSSH-Bridginf-Header.h
+//  HouseAlarm
+//
+//  Created by Roberto Viola on 06/12/14.
+//  Copyright (c) 2014 Roberto Viola. All rights reserved.
+//
+
+#ifndef GR_Bridging_Header_h
+#define GR_Bridging_Header_h
+
+#import <Foundation/Foundation.h>
+#import <ifaddrs.h>
+
+#endif
